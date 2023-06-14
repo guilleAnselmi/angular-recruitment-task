91df44ad8e6c261d2d9b82cad9fc7bdd95b73ffa
Guillermo O. Anselmi <guillermo.anselmi94@gmail.com>
2023-06-14 23:19:59 +0200

    added roboto font and temp links to navigate, also added enviroments and api service for fetching data

--- app/src/api.service.ts
+++ app/src/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from './environments/environment.development';
+import { User } from './models/user.type';
+import { Observable } from 'rxjs';
+import { Country } from './models/country.type';
+import { Payment } from './models/payment.type';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor() { }
+  private apiUrl = environment.API_URL;
+  constructor(private httpClient: HttpClient) {}
+  fetchUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.apiUrl}/users`);
+  }
+  fetchCountries(): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.apiUrl}/countries`);
+  }
+  fetchPayments(): Observable<Payment[]> {
+    return this.httpClient.get<Payment[]>(`${this.apiUrl}/payment`);
+  }
 }
