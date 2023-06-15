892221cb74f18031d9ad0e86725fc0aafe18e556
Guillermo O. Anselmi <guillermo.anselmi94@gmail.com>
2023-06-16 00:23:26 +0200

    refactor countries to use countries service

--- app/src/countries/countries.component.ts
+++ app/src/countries/countries.component.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from 'src/reusable/list/list.component';
 import { Country } from 'src/countries/models/country.type';
 import { Observable, map, of } from 'rxjs';
-import { ApiService } from 'src/api.service';
 import { ListItemComponent } from 'src/reusable/list-item/list-item.component';
 import { CountryItemComponent } from './country-item/country-item.component';
+import { CountriesService } from './services/countries.service';
 
 @Component({
   selector: 'app-countries',
@@ -20,14 +20,16 @@ import { CountryItemComponent } from './country-item/country-item.component';
   styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit {
-  apiService = inject(ApiService);
+  countriesService = inject(CountriesService);
   countries$: Observable<Country[]> = of([]);
   title = 'Manage Countries';
   placeholder = 'Search Countries';
 
   ngOnInit(): void {
-    this.countries$ = this.apiService
-      .fetchCountries()
-      .pipe(map((val) => val.splice(0, 10)));
+    this.countries$ = this.countriesService.getCountriesFiltered();
+  }
+
+  onSearch(searchTerm: string) {
+    this.countriesService.search(searchTerm);
   }
 }
